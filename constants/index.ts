d0d4a5504d0d1f1c601087d4bce108f53bf9579c
alok-more <alokmore1903@gmail.com>
2025-11-07 09:41:55 +0530

    feat: added school information in education section

--- constants/index.ts
+++ constants/index.ts
@@ -1,44 +1,56 @@
 export const bio =
   "My name is Alok More.  Proactive and detail-oriented Software Developer with strong experience in frontend development, Web3 applications, and IoT-based systems. Demonstrated expertise in building scalable and responsive web applications using modern frameworks such as React.js, Next.js, TypeScript, HTML, CSS, JavaScript. Passionate about solving complex technical problems, continuously learning emerging technologies, and contributing to impactful software products.";
 
-
 export const skills = [
-  { name: "Frontend", items: ["React.js", "Next.js", "Tailwind CSS", "JavaScript", "HTML5", "CSS"] },
+  {
+    name: "Frontend",
+    items: [
+      "React.js",
+      "Next.js",
+      "Tailwind CSS",
+      "JavaScript",
+      "HTML5",
+      "CSS",
+    ],
+  },
   { name: "Backend", items: ["Node.js", "Express", "TypeScript"] },
   { name: "Database", items: ["MongoDB", "PostgreSQL", "MySQL", "Supabase"] },
   // { name: "CMS & Headless CMS", items: ["WordPress", "Strapi.js"] },
-  { name: "Tools & Platform", items: ["Git", "GitHub","JIRA", "Postman", "VS Code", "Figma"] },
+  {
+    name: "Tools & Platform",
+    items: ["Git", "GitHub", "JIRA", "Postman", "VS Code", "Figma"],
+  },
   { name: "Programming Languages", items: ["C", "Java", "Python"] },
   // { name: "Computer Fundamentals", items: ["Operating System: Linux, Windows", "Object-Oriented Programming"] },
 ];
 
 export const experience = [
-    {
+  {
     title: "Trainee / Intern Software Developer",
     company: "AYANWORKS Technology Solutions Private Limited",
     duration: "July 2024 - October 2025",
     description: [
-       "Contributed to developing scalable, responsive web apps using React.js, Next.js, and TypeScript.",
-       "Collaborated within Agile teams and participated in code reviews, editing, debugging, and documentation.",
-       "Gained exposure to REST API development and Git-based version control workflows.",
-       "Developed curiosity-driven features improving component reusability and UI performance."
+      "Contributed to developing scalable, responsive web apps using React.js, Next.js, and TypeScript.",
+      "Collaborated within Agile teams and participated in code reviews, editing, debugging, and documentation.",
+      "Gained exposure to REST API development and Git-based version control workflows.",
+      "Developed curiosity-driven features improving component reusability and UI performance.",
     ],
   },
 
   {
     title: "Intern Software Developer",
     company: "Protek Instruments Private Limited",
-    duration:  "June 2023 - August 2023",
+    duration: "June 2023 - August 2023",
     description: [
-       "Built the backend using Node.js and MySQL to create, read, update, and delete (CRUD) data for IoT devices.",
-       "Added features to log real-time sensor data, making it easier to track and analyze device information.",
-        "Set up RS485 and RS232 communication protocols using Modbus Poll and ModScan32 to connect with industrial machines."
+      "Built the backend using Node.js and MySQL to create, read, update, and delete (CRUD) data for IoT devices.",
+      "Added features to log real-time sensor data, making it easier to track and analyze device information.",
+      "Set up RS485 and RS232 communication protocols using Modbus Poll and ModScan32 to connect with industrial machines.",
     ],
   },
-]
+];
 
 export const education = [
-    {
+  {
     collegeName: "Vishwakarma University",
     department: "B.Tech Computer Science Engineering",
     duration: "August 2022 - October 2025",
@@ -51,35 +63,72 @@ export const education = [
     duration: "June 2019 - May 2022",
     cgpa: "79.92%",
   },
-]
+
+  {
+    collegeName: "Vishwakarma Vidyalaya",
+    department: "10th SSC",
+    duration: "June 2018 - May 2019",
+    cgpa: "87.2%",
+  },
+];
 
 export const projects = [
-    {
+  {
     title: "EDUCREDS - Education Domain",
     description:
       "EduCreds is an education-focused platform that provides Design Studio, Issuance, and Micro Credentials features: Users can create certificates or mark sheets using the Design Studio or directly upload HTML versions for issuance. Students can log in, make payments, and access their credentials through the Sovio Wallet.",
-    techs: [ "React.js", "Next.js", "Tailwind CSS", "TypeScript", "Figma", "Supabase"],
+    techs: [
+      "React.js",
+      "Next.js",
+      "Tailwind CSS",
+      "TypeScript",
+      "Figma",
+      "Supabase",
+    ],
     link: "https://educreds.sovio.id/",
   },
   {
     title: "IoT based Data Logging System",
     description:
       "An IoT-based system for real-time data collection, monitoring, and logging from Modbus-enabled devices. Built with Node.js, Modbus-RTU, and custom Admin & Operator dashboards for efficient management and operation.",
-    techs: ["Next.js", "Node.js", "React.js", "TailwindCSS", "Modbus", "modbus-rtu", "Supabase"],
+    techs: [
+      "Next.js",
+      "Node.js",
+      "React.js",
+      "TailwindCSS",
+      "Modbus",
+      "modbus-rtu",
+      "Supabase",
+    ],
     link: "https://github.com/alok-more/Data-Logging-System",
   },
   {
     title: "PinaVault: VC Based File Access",
     description:
       "PinaVault is a privacy-focused file access platform that leverages Verifiable Credentials (VCs) and decentralized storage systems like IPFS and Pinata. Built with Next.js, Tailwind CSS, and TypeScript, the frontend offers a clean, responsive, and user-friendly interface.",
-    techs: ["Next.js", "TypeScript", "TailwindCSS", "Supabase", "Pinata", "CREDEBL"],
+    techs: [
+      "Next.js",
+      "TypeScript",
+      "TailwindCSS",
+      "Supabase",
+      "Pinata",
+      "CREDEBL",
+    ],
     link: "https://github.com/alok-more/PinaVault",
   },
   {
     title: "Gemini PDF Chatbot using Streamlit",
     description:
       "This project is a simple PDF-based chatbot built with Streamlit and Google Gemini API. It allows users to upload a PDF, ask questions about its content, and receive intelligent answers powered by Gemini AI and FAISS vector search.",
-    techs: ["python", "google", "pypdf2", "faiss", "streamlit", "generative-ai", "langchain"],
+    techs: [
+      "python",
+      "google",
+      "pypdf2",
+      "faiss",
+      "streamlit",
+      "generative-ai",
+      "langchain",
+    ],
     link: "https://github.com/alok-more/my-first-chatbot",
   },
   {
@@ -94,7 +143,8 @@ export const projects = [
 export const infos = {
   fullName: "Alok Dadasaheb More",
   status: "Full Stack Developer",
-  shortBio: "Passionate about building web applications & exploring emerging technologies.",
+  shortBio:
+    "Passionate about building web applications & exploring emerging technologies.",
   avatar: "/avtar.jpg",
   mail: "alokmore1903@gmail.com",
   linkedin: "https://www.linkedin.com/in/alok-more-02a65220a/",
